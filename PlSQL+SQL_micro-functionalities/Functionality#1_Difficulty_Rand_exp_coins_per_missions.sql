cbc282dec2acddfcae6ee32a329e57c9d62e7912
bodoz <118077974+zainea-bogdan@users.noreply.github.com>
2025-04-26 18:09:28 +0300

    new feature
    calculating exp adn coins and lvl for each team

--- PlSQL+SQL_micro-functionalities/Functionality#1_Difficulty_Rand_exp_coins_per_missions.sql
+++ PlSQL+SQL_micro-functionalities/Functionality#1_Difficulty_Rand_exp_coins_per_missions.sql
@@ -145,3 +145,164 @@ begin
    ) * 0.3);
    return valoare;
 end;
+
+/*LE DAU DROP CA SA LE POT ADAUGA MAI JOS IN PACHETE*/
+drop procedure validare_existenta_coloana_dificultate_si_alocare_valori;
+drop procedure generare_random_experience;
+drop procedure generare_random_reward_coins;
+drop procedure generare_si_afisare_exp_reward_dificultate;
+drop function formula_calc_based_on_dificulatate_si_regiune;
+
+/*CREATING THE PACKAGE WITH ALL THIS PROCEDURES AND Formula FUNCTION: */
+create or replace package generating_random_dificulty_experience_rewards_per_main_quest as
+   procedure validare_existenta_coloana_dificultate_si_alocare_valori;
+   procedure generare_random_experience;
+   procedure generare_random_reward_coins;
+   procedure generare_si_afisare_exp_reward_dificultate;
+   function formula_calc_based_on_dificulatate_si_regiune (
+      dificultate integer,
+      id_region   integer
+   ) return integer;
+end;
+
+create or replace package body generating_random_dificulty_experience_rewards_per_main_quest is
+   /*PRIMA PROCEDURA*/
+   procedure validare_existenta_coloana_dificultate_si_alocare_valori as
+      v_verifica number;
+      no_column_dificultate exception;
+   begin
+      select count(*)
+        into v_verifica
+        from all_tab_columns
+       where table_name = upper('main_quest')
+         and column_name = upper('dificultate');
+
+    --daca exista coloana facem mai departe update-ul random de dificultate a misiunilor
+      if v_verifica != 0 then
+      --setare pentru regiune 5101
+         execute immediate 'update main_quest
+         set
+         dificultate = round(dbms_random.value(
+            1,
+            10
+         ))
+       where id_region = 5101';
+       --setare pentru regiune 5102
+         execute immediate 'update main_quest
+         set
+         dificultate = round(dbms_random.value(
+            1,
+            10
+         ))
+       where id_region = 5102';
+        --setare pentru regiune 5103
+         execute immediate 'update main_quest
+         set
+         dificultate =round(dbms_random.value(
+            1,
+            10
+         ))
+       where id_region = 5103';
+      else
+         raise no_column_dificultate;
+      end if;
+
+   exception
+      when no_column_dificultate then
+         execute immediate 'ALTER TABLE main_quest ADD (dificultate integer DEFAULT 0 not null)';
+         dbms_output.put_line('S-a creat coloana de dificultate. Te rog sa mai rulezi o data aceasta procedura pentru alocare de valori in noua coloana creata'
+         );
+         validare_existenta_coloana_dificultate_si_alocare_valori;
+   end;
+
+   /*A DOUA PROCEDURA*/
+
+   procedure generare_random_experience as
+   begin
+      update main_quest
+         set
+         main_quest_experience_points = round(dificultate * 600 * 0.7 +(
+            case
+               when id_region = 5101 then
+                  1000
+               when id_region = 5102 then
+                  700
+               when id_region = 5103 then
+                  700
+               else 500
+            end
+         ) * 0.3);
+   end;
+
+
+    /*A TREIA PROCEDURA*/
+
+   procedure generare_random_reward_coins as
+   begin
+      update main_quest
+         set
+         main_quest_reward = round(dificultate * 1000 * 0.7 +(
+            case
+               when id_region = 5101 then
+                  1000
+               when id_region = 5102 then
+                  700
+               when id_region = 5103 then
+                  700
+               else 500
+            end
+         ) * 0.3);
+   end;
+    /*A PATRA PROCEDURA: */
+   procedure generare_si_afisare_exp_reward_dificultate as
+      cursor c is
+      (
+         select id_main_quest,
+                dificultate,
+                main_quest_experience_points,
+                main_quest_reward
+           from main_quest
+      );
+   begin
+      validare_existenta_coloana_dificultate_si_alocare_valori;
+      generare_random_experience;
+      generare_random_reward_coins;
+      for v in c loop
+         dbms_output.put_line('id_misiune: '
+                              || v.id_main_quest
+                              || '|  dificultate: '
+                              || v.dificultate
+                              || '|  exp: '
+                              || v.main_quest_experience_points
+                              || '|  coins: '
+                              || v.main_quest_reward);
+      end loop;
+   end;
+
+    /*SI FUNCTIA */
+   function formula_calc_based_on_dificulatate_si_regiune (
+      dificultate integer,
+      id_region   integer
+   ) return integer as
+      valoare integer;
+   begin
+      valoare := round(dificultate * 1000 * 0.7 +(
+         case
+            when id_region = 5101 then
+               1000
+            when id_region = 5102 then
+               700
+            when id_region = 5103 then
+               700
+            else 500
+         end
+      ) * 0.3);
+      return valoare;
+   end;
+end;
+
+execute generating_random_dificulty_experience_rewards_per_main_quest.generare_random_experience;
+commit;
+
+select *
+  from main_quest;
---
+++ PlSQL+SQL_micro-functionalities/Functionality#2_Check-Unchecked-Missions_Exp+Coins_per_team_Create_columns_exp_coins_in_teams.sql
@@ -0,0 +1,218 @@
+/*Functionality #2*/
+/*Idee de baza: vreau sa contruiesc un pachet de proceduri si functii care fac urmatoarele chestii:
+- 1 procedura care imi da update pe coloaan cu status misiune care imi zice daca misiunea este teminata(1) sau neincheiata(0)
+- 1 procedura ce verifica si creeaza coloana de reward, exp per echipa si plus o coloana de level unde se calculeaza pe baza unei formule lvl echipei in functie de exp 
+- 1 procedura care imi calculeaza exp si coins per echipa si imi stocheaza fiecare valoare intr-o structura de date si apoi din structura de date eu iau si pun in tabela :)
+- */
+
+
+   set SERVEROUTPUT on;
+   /*procedura care imi da update pe coloaan cu status misiune */
+create or replace procedure random_completare_main_quests as
+begin
+   update main_quest
+      set
+      main_quest_status = round(dbms_random.value(
+         0,
+         1
+      ));
+end;
+
+/*functie de calculare level echipa in functie de exp castigat*/
+create or replace function calculare_level (
+   experience integer
+) return integer as
+   lvl integer;
+begin
+   lvl := trunc(experience / 1000);
+   return lvl;
+end;
+
+/*procedura care imi creeaza coloanele de exp si coins in tabela de charcater party si verifica daca ele exisdta deja*/
+create or replace procedure validare_coloane_exp_coins_echipe as
+   validare_exp_column   integer;
+   validare_coins_column integer;
+   validare_lvl_column   integer;
+   no_exp_column exception;
+   no_coins_column exception;
+   no_lvl_column exception;
+begin
+/*verific daca exista coloanad e exp echipa in cadrul tabelei de character party:)*/
+   select count(*)
+     into validare_exp_column
+     from all_tab_columns
+    where table_name = upper('character_party')
+      and column_name = upper('exp_echipa');
+
+   if ( validare_exp_column = 0 ) then
+      raise no_exp_column;
+   else
+    /*verific pentru coloaan de coins*/
+
+      select count(*)
+        into validare_coins_column
+        from all_tab_columns
+       where table_name = upper('character_party')
+         and column_name = upper('coins_echipa');
+
+      if ( validare_coins_column = 0 ) then
+         raise no_coins_column;
+      else
+      /*verific pentru coloana de lvl echipa */
+         select count(*)
+           into validare_lvl_column
+           from all_tab_columns
+          where table_name = upper('character_party')
+            and column_name = upper('lvl_echipa');
+
+         if ( validare_lvl_column = 0 ) then
+            raise no_lvl_column;
+         end if;
+      end if;
+   end if;
+
+
+exception
+   when no_exp_column then
+      execute immediate 'ALTER TABLE character_party ADD (exp_echipa integer DEFAULT 0 not null)';
+      dbms_output.put_line('S-a creat coloana de exp echipa. Reload');
+      validare_coloane_exp_coins_echipe;
+   when no_coins_column then
+      execute immediate 'ALTER TABLE character_party ADD (coins_echipa integer DEFAULT 0 not null)';
+      dbms_output.put_line('S-a creat coloana de coins echipa. Reload');
+      validare_coloane_exp_coins_echipe;
+   when no_lvl_column then
+      execute immediate 'ALTER TABLE character_party ADD (lvl_echipa integer DEFAULT 0 not null)';
+      dbms_output.put_line('S-a creat coloana de lvl echipa.Reload');
+      validare_coloane_exp_coins_echipe;
+end;
+
+
+
+create or replace procedure calculare_exp_echipa as
+   type exp_array is
+      table of integer index by pls_integer;
+   structura_de_date_exp exp_array;
+   indexare              integer := 6001;
+   temp                  integer;
+begin
+   loop
+      begin
+         select round(sum(mq.main_quest_experience_points)) as expe
+           into temp
+           from main_quest mq
+          inner join character_party cp
+         on cp.id_party = mq.id_party
+          where main_quest_status = 1
+            and cp.id_party = indexare
+          group by cp.id_party;
+
+         structura_de_date_exp(indexare) := temp;
+      exception
+         when no_data_found then
+            structura_de_date_exp(indexare) := 0;
+      end;
+      indexare := indexare + 1;
+      exit when indexare > 6009;
+   end loop;
+
+   for indexare in structura_de_date_exp.first..structura_de_date_exp.last loop
+      if structura_de_date_exp.exists(indexare) then
+         update character_party
+            set
+            exp_echipa = structura_de_date_exp(indexare)
+          where indexare = id_party;
+      end if;
+   end loop;
+end;
+
+execute calculare_exp_echipa;
+
+select *
+  from character_party;
+
+
+-- select cp.id_party as party,
+--        round(sum(mq.main_quest_experience_points)) as exp
+--   from main_quest mq
+--  inner join character_party cp
+-- on cp.id_party = mq.id_party
+--  where main_quest_status = 1
+--  group by cp.id_party;
+
+
+/*procedura care calculeaza coins, le baga intr o sturctura de date si apoi le baga in tabel una cate una */
+create or replace procedure calculare_coins_echipa as
+   type coins_array is
+      table of integer index by pls_integer;
+   structura_de_date_exp coins_array;
+   indexare              integer := 6001;
+   temp                  integer;
+begin
+   loop
+      begin
+         select round(sum(mq.main_quest_reward)) as expe
+           into temp
+           from main_quest mq
+          inner join character_party cp
+         on cp.id_party = mq.id_party
+          where main_quest_status = 1
+            and cp.id_party = indexare
+          group by cp.id_party;
+
+         structura_de_date_exp(indexare) := temp;
+      exception
+         when no_data_found then
+            structura_de_date_exp(indexare) := 0;
+      end;
+      indexare := indexare + 1;
+      exit when indexare > 6009;
+   end loop;
+
+   for indexare in structura_de_date_exp.first..structura_de_date_exp.last loop
+      if structura_de_date_exp.exists(indexare) then
+         update character_party
+            set
+            coins_echipa = structura_de_date_exp(indexare)
+          where indexare = id_party;
+      end if;
+   end loop;
+end;
+
+execute calculare_coins_echipa;
+select *
+  from character_party;
+
+-- select cp.id_party,
+--        round(sum(mq.main_quest_reward)) as expe
+--   from main_quest mq
+--  inner join character_party cp
+-- on cp.id_party = mq.id_party
+--  inner join caracter car
+-- on car.id_character_party = cp.id_party
+--  where main_quest_status = 1
+--  group by cp.id_party;
+
+
+/*procedura care calculeaza lvl echipei */
+
+create or replace procedure lvl_procedure as
+   cursor c is
+   (
+      select id_party,
+             exp_echipa experience
+        from character_party
+   );
+begin
+   for v in c loop
+      update character_party
+         set
+         lvl_echipa = calculare_level(v.experience)
+       where id_party = v.id_party;
+   end loop;
+end;
+
+execute lvl_procedure;
+
+select *
+  from character_party;
